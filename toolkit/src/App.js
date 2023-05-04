bd1d8ffb33494876f6ea6c4f7dd7654313842c1c
Hyunmin Choi <hymn9613@gmail.com>
2023-05-04 15:42:29 +0900

    카운터 redux toolkit 적용

--- toolkit/src/App.js
+++ toolkit/src/App.js
@@ -1,27 +1,33 @@
 import { Provider, useDispatch, useSelector } from "react-redux";
-import { createStore } from "redux";
+import { createSlice, configureStore } from "@reduxjs/toolkit";
 
-const initialState = { value: 0 };
+const counterSlice = createSlice({
+  name: "counterSlice",
+  initialState: { value: 0 },
+  reducers: {
+    up: (state, action) => {
+      // state.value = state.value + action.step;
+      state.value = state.value + action.payload;
+    },
+  },
+});
 
-const reducer = (state, action) => {
-  if (action.type === "up") {
-    return { ...state, value: state.value + action.step };
-  }
-
-  return state;
-};
-
-const store = createStore(reducer, initialState);
+const store = configureStore({
+  reducer: {
+    counter: counterSlice.reducer,
+  },
+});
 
 const Counter = () => {
   const dispatch = useDispatch();
-  const count = useSelector((state) => state.value);
+  const count = useSelector((state) => state.counter.value);
 
   return (
     <div>
       <button
         onClick={() => {
-          dispatch({ type: "up", step: 2 });
+          // dispatch({ type: "counterSlice/up", step: 2 });
+          dispatch(counterSlice.actions.up(2));
         }}
       >
         +
